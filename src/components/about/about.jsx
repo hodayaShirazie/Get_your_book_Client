9c5ffec7440e4e78c7b5bf398439578b77b0f0ba
hodaya <hoddaya.sh2003@gmail.com>
2025-05-13 21:00:46 +0300

    fixed import

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './about.css';
+import './About.css';
 import { useNavigate } from 'react-router-dom';
 import BackToHomeButton from '../BackToHomeButton/BackToHomeButton';
 
