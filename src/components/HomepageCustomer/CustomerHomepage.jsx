42c65621ab7ecdaccea5c2edf3575d0a5be36255
hodaya <hoddaya.sh2003@gmail.com>
2025-05-16 15:45:57 +0300

    search for product clickable icon

--- src/components/HomepageCustomer/CustomerHomepage.jsx
+++ src/components/HomepageCustomer/CustomerHomepage.jsx
@@ -147,7 +147,7 @@ if (sortOrder === 'low-high') {
             />
             <svg
               className="search-icon"
-              onClick={() => handleSearch()}
+              onClick={handleSearch}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               width="18"
